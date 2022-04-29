385a51eb5036b8beb4493d969746f115e4824d65
Grace Li <gracejieyi@gmail.com>
2022-04-29 13:18:53 -0700

    ugggghhh

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,22 @@
 use frenderer::animation::{AnimationSettings, AnimationState};
 use frenderer::assets::{AnimRef, MeshRef};
 use frenderer::camera::Camera;
+//use frenderer::image::Image;
 use frenderer::renderer::flat::SingleRenderState as FFlat;
 use frenderer::renderer::skinned::SingleRenderState as FSkinned;
 use frenderer::renderer::sprites::SingleRenderState as FSprite;
 use frenderer::renderer::textured::{Model, SingleRenderState as FTextured};
 use frenderer::types::*;
 use frenderer::{Engine, FrendererSettings, Key, Result, SpriteRendererSettings};
+use rand::Rng;
 use scene3d::types::{self, *};
 use std::rc::Rc;
-use rand::Rng;
-
 
 const DT: f64 = 1.0 / 60.0;
 const SPEED: f32 = 0.25;
 const ROOMSIZE: f32 = 60.0;
 const COLLISION_RADIUS: f32 = 1.0;
 const SCALE: f32 = 10.0;
-
-//game states: main screen, instruction, playing, finalscreen
-//make one room first with lockedchest
-//if goes in through a door, generate a new room add it to rooms and
-//have a checker that makes sure rooms.len < max_room and rooms.len != key_index
-//have the character (collision?) get the key and put it in inventory
-//go through rooms until main room
 
 #[derive(Clone)]
 
@@ -35,6 +28,7 @@ pub struct GameState {
     //pub inventory: Vec<GameObject>,
     pub rooms: Vec<Room>,
     pub is_finished: bool,
+    pub gameplaystate: GameplayState,
 }
 
 #[derive(Clone)]
@@ -79,13 +73,20 @@ impl Sprite {
     pub fn move_by(&mut self, vec: Vec3) {
         self.trf.append_translation(vec);
     }
-    pub fn check_collisions(&mut self, door: Door) -> bool{
+    pub fn check_collisions(&mut self, door: Door) -> bool {
         let door_worldspace = get_trf(door.direction, ROOMSIZE, SCALE);
-        return (self.trf.translation.x - door_worldspace.translation.x).abs() <= self.size.x/2.
-        && (self.trf.translation.z - door_worldspace.translation.z).abs() <= COLLISION_RADIUS;
+        return (self.trf.translation.x - door_worldspace.translation.x).abs() <= self.size.x / 2.
+            && (self.trf.translation.z - door_worldspace.translation.z).abs() <= COLLISION_RADIUS;
         // if self.cel.contains(door_worldspace.translation)
-    
     }
+}
+
+#[derive(Clone, PartialEq)]
+pub enum GameplayState {
+    Mainscreen,
+    Instructions,
+    Play,
+    FinalScreen,
 }
 struct World {
     camera: Camera,
@@ -116,12 +117,23 @@ impl frenderer::World for World {
         let dscale = input.key_axis(Key::E, Key::R) * 1.0 * DT as f32;
         let rot = Rotor3::from_euler_angles(roll, pitch, yaw);
 
+        if self.state.gameplaystate == GameplayState::Mainscreen {
+            //goal is just to get an image on the screen that says "dorm game"
+            // let main_image = Image::new(10, 10);
+            // main_image.from_file(std::path::Path::new("content/robot.png"));
+        } else if self.state.gameplaystate == GameplayState::Play {
+            //to get this to work we need to modify frenderer engine's ".render bit"
+            //so that it doesn't render everything onto the screen
+        }
+
+        //trying to get it to move
         for obj in self.things.iter_mut() {
             obj.trf.append_rotation(rot);
             obj.trf.scale = (obj.trf.scale + dscale).max(0.01);
             // dbg!(obj.trf.rotation);
             obj.tick_animation();
         }
+
         for s in self.sprites.iter_mut() {
             // s.trf.append_rotation(rot);
             // s.size.x += dscale;
@@ -139,13 +151,10 @@ impl frenderer::World for World {
                 s.move_by(Vec3::new(SPEED, 0.0, 0.0));
             }
             for door in self.state.rooms[self.state.current_room].doors.iter() {
-                if( s.check_collisions(*door)){
+                if (s.check_collisions(*door)) {
                     self.state.current_room = door.target;
-                    s.trf.translation = get_trf(
-                        door.direction,
-                        ROOMSIZE,
-                        SCALE).translation;
-                    dbg!({""}, self.state.current_room);
+                    s.trf.translation = get_trf(door.direction, ROOMSIZE, SCALE).translation;
+                    dbg!({ "" }, self.state.current_room);
                 }
             }
         }
@@ -170,6 +179,7 @@ impl frenderer::World for World {
         rs.set_camera(self.camera);
         //render the doors in the correct positions
         let door_list = &self.state.rooms[self.state.current_room].doors;
+        let mut tex_render_key = 0 as usize;
         if door_list.len() > 0 {
             rs.render_textured(
                 0 as usize,
@@ -217,22 +227,11 @@ impl frenderer::World for World {
 
         for (obj_i, obj) in self.things.iter_mut().enumerate() {
             rs.render_skinned(
-                obj_i,
+                5 as usize,
                 obj.model.clone(),
                 FSkinned::new(obj.animation, obj.state, obj.trf),
             );
         }
-
-        // grace's render skinned it is not good code
-        // rs.render_skinned(
-        //     3 as usize,
-        //     self.sprites[0].model,
-        //     FSkinned::new(
-        //         self.sprites[0].sprite_object.animation,
-        //         self.sprites[0].sprite_object.state,
-        //         self.sprites[0].sprite_object.trf,
-        //     ),
-        // );
 
         //render the sprites
         for (s_i, s) in self.sprites.iter_mut().enumerate() {
@@ -241,9 +240,11 @@ impl frenderer::World for World {
         // for (m_i, m) in self.flats.iter_mut().enumerate() {
         //     rs.render_flat(m_i, m.model.clone(), FFlat::new(m.trf));
         // }
-        // for (t_i, t) in self.textured.iter_mut().enumerate() {
-        //     rs.render_textured(t_i, t.model.clone(), FTextured::new(t.trf));
-        // }
+
+        //tricky: if rendering somehting the "key" needs to be unique for each one if it's the same rs function
+        for (t_i, t) in self.textured.iter_mut().enumerate() {
+            rs.render_textured(4 as usize, t.model.clone(), FTextured::new(t.trf));
+        }
     }
 }
 fn main() -> Result<()> {
@@ -275,28 +276,10 @@ fn main() -> Result<()> {
     let door = engine.load_textured(std::path::Path::new("content/door.fbx"))?;
     let door_model = engine.create_textured_model(door, vec![tex]);
 
-    //code for a textured model
-    // let sprite_meshes = engine.load_textured(std::path::Path::new("content/characterSmall.fbx"))?;
-    // let sprite_texture = engine.load_texture(std::path::Path::new("content/robot.png"))?;
-    // let sprite_model = engine.create_textured_model(sprite_meshes, vec![sprite_texture]);
-
-    // let sprite_animation = engine.load_anim(
-    //     std::path::Path::new("content/kick.fbx"),
-    //     sprite_meshes[0],
-    //     AnimationSettings { looping: true },
-    //     "Root|Kick",
-    // )?;
-
-    // let sprite_obj = GameObject::new(
-    //     Similarity3::new(
-    //         Vec3::new(0.0, -25.0, 25.0),
-    //         Rotor3::from_euler_angles(0.0, -PI / 2.0, 0.0),
-    //         1.0,
-    //     ),
-    //     sprite_model,
-    //     sprite_animation,
-    //     AnimationState { t: 0.0 },
-    // );
+    // //code for a textured model
+    let sprite_meshes = engine.load_textured(std::path::Path::new("content/characterSmall.fbx"))?;
+    let sprite_texture = engine.load_texture(std::path::Path::new("content/robot.png"))?;
+    let sprite_textured_model = engine.create_textured_model(sprite_meshes, vec![sprite_texture]);
 
     //code for skinned model and gameObject
     let sprite_meshes = engine.load_skinned(
@@ -313,15 +296,16 @@ fn main() -> Result<()> {
     let sprite_texture = engine.load_texture(std::path::Path::new("content/robot.png"))?;
     let sprite_model = engine.create_skinned_model(sprite_meshes, vec![sprite_texture]);
 
+    // sprite gameobject
     let sprite_obj = GameObject::new(
         Similarity3::new(
-            Vec3::new(0.0, -25.0, 25.0),
+            Vec3::new(0.0, 0.0, 0.0),
             Rotor3::from_euler_angles(0.0, -PI / 2.0, 0.0),
             1.0,
         ),
         sprite_model,
         sprite_animation,
-        AnimationState { t: 0.0 },
+        AnimationState { t: 1.0 },
     );
 
     let game_sprite = Sprite {
@@ -333,45 +317,6 @@ fn main() -> Result<()> {
         //sprite_object: sprite_obj,
     };
 
-    // let model = engine.load_textured(std::path::Path::new("content/characterSmall.fbx"))?;
-    // let char_model = engine.create_textured_model(model, vec![tex]);
-
-    // let meshes = engine.load_skinned(
-    //     std::path::Path::new("content/characterSmall.fbx"),
-    //     &["RootNode", "Root"],
-    // )?;
-    // let animation = engine.load_anim(
-    //     std::path::Path::new("content/kick.fbx"),
-    //     meshes[0],
-    //     AnimationSettings { looping: true },
-    //     "Root|Kick",
-    // )?;
-    // let model = engine.create_skinned_model(meshes, vec![tex]);
-
-    //LA LA LA LA LA
-
-    // let door_1 = Door {
-    //     direction: Direction::North,
-    //     target: 0,
-    // };
-    // let door_2 = Door {
-    //     direction: Direction::East,
-    //     target: 0,
-    // };
-    // let door_3 = Door {
-    //     direction: Direction::South,
-    //     target: 0,
-    // };
-    // let door_4 = Door {
-    //     direction: Direction::West,
-    //     target: 0,
-    // };
-    // let starting_room = Room {
-    //     doors: vec![door_1, door_2, door_3, door_4],
-    //     // floor: engine.load_texture(std::path::Path::new("content/robot.png"))?,
-    //     objects: vec![],
-    // };
-
     let rooms = generate_rooms(5);
 
     let game_state = GameState {
@@ -381,27 +326,23 @@ fn main() -> Result<()> {
         //inventory: vec![],
         rooms: rooms,
         is_finished: false,
+        gameplaystate: GameplayState::Play,
     };
 
     let world = World {
         camera,
-        things: vec![
-            sprite_obj, // GameObject {
-                       //     trf: Similarity3::new(Vec3::new(0.0, 0.0, 0.0), Rotor3::identity(), 1.0),
-                       //     model,
-                       //     animation,
-                       //     state: AnimationState { t: 0.0 },
-                       // }
-        ],
+        //game object isn't rendering :(
+        things: vec![sprite_obj], //vec![sprite_obj],
         sprites: vec![game_sprite],
         flats: vec![],
-        textured: vec![
-        // Textured {
-        //     trf: Similarity3::new(Vec3::new(0.0, 0.0, 0.0),Rotor3::from_euler_angles(0.0, -PI / 2.0, 0.0), 1.0),
-        //     model: char_model,
-        //     name: String::from("Character")
-        // }
-        ],
+        textured: vec![],
+        //here is the code for the standard character model
+        //no animations yet
+        //vec![Textured {
+        //     trf: Similarity3::new(Vec3::new(0.0, 0.0, 0.0), Rotor3::identity(), 0.05),
+        //     model: char_model.clone(),
+        //     name: String::from("Character"),
+        // }],
         door1: Textured {
             trf: Similarity3::new(
                 Vec3::new(0.0, 0.0, 0.0),
@@ -472,7 +413,7 @@ fn get_trf(dir: Direction, room_size: f32, scale: f32) -> Similarity3 {
 fn generate_rooms(num_rooms: u32) -> Vec<Room> {
     let mut vec = Vec::<Room>::new();
     let mut rng = rand::thread_rng();
-    for n in 0..num_rooms{
+    for n in 0..num_rooms {
         let doors = generate_doors(rng.gen_range(1..4), num_rooms);
         let room = Room::new(doors, vec![]);
         vec.push(room);
@@ -480,7 +421,7 @@ fn generate_rooms(num_rooms: u32) -> Vec<Room> {
     return vec;
 }
 
-fn generate_doors(num_doors: u32, num_rooms: u32) -> Vec<Door>{
+fn generate_doors(num_doors: u32, num_rooms: u32) -> Vec<Door> {
     let mut vec = Vec::<Door>::new();
     let mut dirs = Vec::<Direction>::new();
     let mut rng = rand::thread_rng();
@@ -491,7 +432,7 @@ fn generate_doors(num_doors: u32, num_rooms: u32) -> Vec<Door>{
     while length < num_doors as usize {
         let next_direction = get_dir(rng.gen_range(0..3));
         for direction in &*dirs {
-            if next_direction == *direction{
+            if next_direction == *direction {
                 contains = true;
             }
         }
@@ -501,9 +442,8 @@ fn generate_doors(num_doors: u32, num_rooms: u32) -> Vec<Door>{
             dbg!(length);
         }
         contains = false;
-
     }
-    for n in 0..num_doors{
+    for n in 0..num_doors {
         let mut target = rng.gen_range(0..num_rooms as usize);
         while target == n as usize {
             target = rng.gen_range(0..num_rooms as usize);
@@ -524,7 +464,6 @@ fn get_dir(num: u32) -> Direction {
     }
 }
 
-
 fn get_spawn_dir(dir: Direction) -> Direction {
     match dir {
         Direction::North => Direction::South,
@@ -534,3 +473,40 @@ fn get_spawn_dir(dir: Direction) -> Direction {
         Other => Direction::West,
     }
 }
+
+// let model = engine.load_textured(std::path::Path::new("content/characterSmall.fbx"))?;
+// let char_model = engine.create_textured_model(model, vec![tex]);
+
+// let meshes = engine.load_skinned(
+//     std::path::Path::new("content/characterSmall.fbx"),
+//     &["RootNode", "Root"],
+// )?;
+// let animation = engine.load_anim(
+//     std::path::Path::new("content/kick.fbx"),
+//     meshes[0],
+//     AnimationSettings { looping: true },
+//     "Root|Kick",
+// )?;
+// let model = engine.create_skinned_model(meshes, vec![tex]);
+
+// let door_1 = Door {
+//     direction: Direction::North,
+//     target: 0,
+// };
+// let door_2 = Door {
+//     direction: Direction::East,
+//     target: 0,
+// };
+// let door_3 = Door {
+//     direction: Direction::South,
+//     target: 0,
+// };
+// let door_4 = Door {
+//     direction: Direction::West,
+//     target: 0,
+// };
+// let starting_room = Room {
+//     doors: vec![door_1, door_2, door_3, door_4],
+//     // floor: engine.load_texture(std::path::Path::new("content/robot.png"))?,
+//     objects: vec![],
+// };
