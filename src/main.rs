2c965169256b532aaf2bbc2e0bf925d580aaddfc
Grace Li <gracejieyi@gmail.com>
2022-05-03 13:22:34 -0700

    key index

--- src/main.rs
+++ src/main.rs
@@ -726,7 +726,7 @@ fn main() -> Result<()> {
     let game_state = GameState {
         current_room: 0, //index of room in rooms
         max_rooms: 4,
-        key_index: keyidx,
+        key_index: 3,
         //inventory: vec![],
         // rooms: vec![Room::new(vec![0]), Room::new(vec![1])],
         rooms: room_list,
